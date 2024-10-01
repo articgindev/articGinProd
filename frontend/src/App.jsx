e6f709f8e8b51daa93220be74b71392dbdb027cd
gaetanof <franciscogaetano1@gmail.com>
2024-10-01 12:13:39 -0300

    test performance

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,18 @@
-import React from 'react';
-import { Routes, Route, Navigate } from 'react-router-dom';
-import Home from './pages/Home';
-import Cart from './pages/Cart';
-import Age from './pages/Age';
-import Pay from './pages/Pay';
-import Success from './pages/Success';
-
+import React, { Suspense, lazy } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from 'react-router-dom';
 import './App.css';
+
+// Lazy loading de tus componentes
+const Home = lazy(() => import('./pages/Home'));
+const Cart = lazy(() => import('./pages/Cart'));
+const Age = lazy(() => import('./pages/Age'));
+const Pay = lazy(() => import('./pages/Pay'));
+const Success = lazy(() => import('./pages/Success'));
 
 function App() {
   const isAdult = () => {
@@ -15,25 +21,27 @@ function App() {
 
   return (
     <div className="app-container">
-      <Routes>
-        <Route path="/age" element={<Age />} />
-        <Route
-          path="/"
-          element={isAdult() ? <Home /> : <Navigate to="/age" />}
-        />
-        <Route
-          path="/cart"
-          element={isAdult() ? <Cart /> : <Navigate to="/age" />}
-        />
-        <Route
-          path="/pagar/:cartId"
-          element={isAdult() ? <Pay /> : <Navigate to="/" />}
-        />
-        <Route
-          path="/success"
-          element={isAdult() ? <Success /> : <Navigate to="/" />}
-        />
-      </Routes>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/age" element={<Age />} />
+          <Route
+            path="/"
+            element={isAdult() ? <Home /> : <Navigate to="/age" />}
+          />
+          <Route
+            path="/cart"
+            element={isAdult() ? <Cart /> : <Navigate to="/age" />}
+          />
+          <Route
+            path="/pagar/:cartId"
+            element={isAdult() ? <Pay /> : <Navigate to="/" />}
+          />
+          <Route
+            path="/success"
+            element={isAdult() ? <Success /> : <Navigate to="/" />}
+          />
+        </Routes>
+      </Suspense>
     </div>
   );
 }
